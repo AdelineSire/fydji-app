0f088c2d2c74c39bf91707b7c3c478db2202afe1
Adeline <adeline.sire@gmail.com>
2022-04-30 21:43:01 +0200

    Adds route to create a new user and send a confirmation email

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import mongoose from 'mongoose';
+import cors from 'cors';
+
+import createJobs from './createJobs.js';
+import createUser from './createUser.js';
+import sendSubscriptionEmail from './mailing/sendSubscriptionEmail';
 
 dotenv.config();
 const MONGODB_URI = process.env.MONGODB_URI;
@@ -15,11 +20,27 @@ mongoose
 	});
 
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/api/actions/create/jobs', async (req, res) => {
 	const dateStr = req.query.date;
 	await createJobs(dateStr);
 	res.send('New jobs have been created');
+});
+
+app.post('/actions/signup-email', async (req, res) => {
+	const email = req.body.email;
+	const isGmail = email.includes('gmail');
+
+	await createUser(email).then(() =>
+		res.json({
+			success: true,
+			isGmail: isGmail,
+		})
+	);
+	sendSubscriptionEmail(email);
 });
 
 app.listen(process.env.PORT || 3000);
