ce7f79369a2fd1c939eb5da5711151911dc9f046
Adeline <adeline.sire@gmail.com>
2022-04-30 10:03:34 +0200

    Adds extractHostNameFromUrl

---
+++ extractHostNameFromUrl/index.js
@@ -0,0 +1,7 @@
+const extractHostNameFromUrl = (url) => {
+	const urlObject = new URL(url);
+	const hostName = urlObject.hostname.replace('www.', '').replace('.com', '');
+	return hostName;
+};
+
+export default extractHostNameFromUrl;
