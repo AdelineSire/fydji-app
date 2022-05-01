5b05f68f513d46735be1c1090d9a2c5ff5f70e9b
Adeline <adeline.sire@gmail.com>
2022-05-01 21:09:05 +0200

    Changes .env variables import

--- mailing/transporter.js
+++ mailing/transporter.js
@@ -1,5 +1,6 @@
 import nodemailer from 'nodemailer';
-const { GMAIL_USERNAME, GMAIL_PASSWORD } = process.env;
+const GMAIL_USERNAME = process.env.GMAIL_USERNAME;
+const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
 
 const transporter = nodemailer.createTransport({
 	service: 'gmail',
