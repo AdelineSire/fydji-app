1ff5bd11d6a46e7e5137301658446763e7cc1402
Adeline <adeline.sire@gmail.com>
2022-05-13 11:26:20 +0200

    Adds email to sendJobsEmail

--- mailing/getJobsContent.js
+++ mailing/getJobsContent.js
@@ -10,19 +10,23 @@ const API_URL = process.env.API_URL;
 const HOST = process.env.HOST;
 const dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const getJobsContent = async (jobs) => {
+const getTemplateContent = async () => {
+	const templatePath = path.join(dirname, 'template', 'newsletter.handlebars');
+	const templateContent = await fs.readFile(templatePath, 'utf8');
+	return templateContent;
+};
+
+const getJobsContent = async (jobs, email) => {
 	const oJobs = jobs.map((j) => j.toObject());
 	const templateContent = await getTemplateContent();
 	const template = Handlebars.compile(templateContent);
-	const html = template({ jobs: oJobs, host: HOST, apiUrl: API_URL });
+	const html = template({
+		jobs: oJobs,
+		host: HOST,
+		apiUrl: API_URL,
+		email: email,
+	});
 	return html;
-};
-
-const getTemplateContent = async () => {
-	const templatePath = path.join(dirname, 'template', 'newsletter.handlebars');
-
-	const templateContent = await fs.readFile(templatePath, 'utf8');
-	return templateContent;
 };
 
 export default getJobsContent;
--- mailing/sendJobsEmail.js
+++ mailing/sendJobsEmail.js
@@ -1,33 +1,32 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
-import getJobsContent from "./getJobsContent.js";
-import transporter from "./transporter.js";
+import getJobsContent from './getJobsContent.js';
+import transporter from './transporter.js';
 
 dotenv.config();
 const GMAIL_USERNAME = process.env.GMAIL_USERNAME;
 
 const sendEmail = (email, subject, content) => {
-  const mailOptions = {
-    from: GMAIL_USERNAME,
-    to: email,
-    subject: subject,
-    html: content,
-  };
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      console.log("err in transporter.sendMail: ", err);
-    } else {
-      console.log("info: ", info);
-    }
-  });
+	const mailOptions = {
+		from: GMAIL_USERNAME,
+		to: email,
+		subject: subject,
+		html: content,
+	};
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			console.log('err in transporter.sendMail: ', err);
+		} else {
+			console.log('info: ', info);
+		}
+	});
 };
 
-const sendJobsEmail = async (jobs, subject = "Les nouvelles offres", users) => {
-  const content = await getJobsContent(jobs);
-
-  for (const user of users) {
-    sendEmail(user.email, subject, content);
-  }
+const sendJobsEmail = async (jobs, subject = 'Les nouvelles offres', users) => {
+	for (const user of users) {
+		const content = await getJobsContent(jobs, user.email);
+		sendEmail(user.email, subject, content);
+	}
 };
 
 export default sendJobsEmail;
