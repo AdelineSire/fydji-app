0a2352e1e2dbf60c74bd63a714c1de894202cd6c
Adeline <adeline.sire@gmail.com>
2022-05-16 12:48:21 +0200

    Changes email transporter

--- mailing/transporter.js
+++ mailing/transporter.js
@@ -1,17 +1,19 @@
-import dotenv from "dotenv";
-import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
 
 dotenv.config();
 
-const GMAIL_USERNAME = process.env.GMAIL_USERNAME;
-const GMAIL_PASSWORD = process.env.GMAIL_PASSWORD;
+const USERNAME = process.env.USERNAME;
+const PASSWORD = process.env.PASSWORD;
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: GMAIL_USERNAME,
-    pass: GMAIL_PASSWORD,
-  },
+	port: 465,
+	secure: true,
+	host: 'mail.gandi.net',
+	auth: {
+		user: USERNAME,
+		pass: PASSWORD,
+	},
 });
 
 export default transporter;
